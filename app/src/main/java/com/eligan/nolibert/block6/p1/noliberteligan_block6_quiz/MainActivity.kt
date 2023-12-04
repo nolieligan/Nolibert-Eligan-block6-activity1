c437404899a44698b2da60e0c0eb456c8b2f10fa
Nolibert Eligan <nolibertjr.eligan@gmail.com>
2023-12-04 18:10:40 +0800

    Second Commit 1

---
+++ app/src/main/java/com/eligan/nolibert/block6/p1/noliberteligan_block6_quiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eligan.nolibert.block6.p1.noliberteligan_block6_quiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
